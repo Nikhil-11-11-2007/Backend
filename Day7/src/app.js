fad73029a1fb5e92a9042f5944a0086e35af0e98
Nikhil-11-11-2007 <ng364357@gmail.com>
2026-01-30 17:24:01 +0530

    Day 7 complete

---
+++ Day7/src/app.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const noteModel = require("./models/notes.model")
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/notes", async (req, res) => {
+    const { title, description, age } = req.body
+    const notee = await noteModel.create({
+        title, description, age
+    })
+
+    res.status(201).json({
+        message:"Note created successfully",
+        notee
+    })
+})
+
+app.get("/notes", async (req,res) => {
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message: "Note fetched successfully",
+        notes
+    })
+})
+
+module.exports = app
