14803ddf3a8be997ffe040180bce69213afde242
Nikhil-11-11-2007 <ng364357@gmail.com>
2026-02-03 20:09:14 +0530

    Add initial HTML and CSS files for frontend layout
    - Created index.html with basic structure and linked assets
    - Added index-DYWrM5QP.css for styling the application

--- Day9/Backend/src/app.js
+++ Day9/Backend/src/app.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const noteModel = require("./models/note.model")
-
 const cors = require("cors")
+const path = require("path")
 
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use(express.static("./public"))
 
 app.post("/api/notes", async (req, res) => {
     const { title, description } = req.body
@@ -53,6 +54,10 @@ app.patch("/api/notes/:id", async (req, res) => {
         message: "updated successfully",
         note
     })
+})
+
+app.use("*name", (req,res) => {
+    res.sendFile(path.join(__dirname, "..", "/public/index.html"))
 })
 
 module.exports = app
--- Day9/Frontend/frontend/src/App.jsx
+++ Day9/Frontend/frontend/src/App.jsx
@@ -1,25 +1,79 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios"
 
 function App() {
 
   const [notes, setNotes] = useState([])
+  const [editNote, setEditNote] = useState(null)
 
   const getData = async () => {
     const res = await axios.get("http://localhost:3000/api/notes")
     return setNotes(res.data.note)
   }
 
-  getData()
+  useEffect(() => {
+    getData()
+  }, [])
+
+  function submitHandeler(e) {
+    e.preventDefault()
+    const { title, description } = e.target.elements
+    axios.post("http://localhost:3000/api/notes", {
+      title: title.value,
+      description: description.value
+    })
+      .then((res) => {
+        res.data
+        getData()
+      })
+
+  }
+
+  function updateHandeler(e) {
+    e.preventDefault()
+    const description = e.target.elements.newDescription.value
+    axios.patch("http://localhost:3000/api/notes/" + editNote, { description })
+      .then(() => {
+        e.target.reset()
+        setEditNote(null)
+        getData()
+      })
+  }
+
+  function deleteHandeler(noteId) {
+    console.log(noteId);
+    axios.delete("http://localhost:3000/api/notes/" + noteId)
+      .then((res) => {
+        console.log(res.data);
+        getData()
+      })
+  }
 
   return (
     <>
+      {/* submit handeler */}
+      <form onSubmit={submitHandeler} className='note-create-form'>
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter description' />
+        <button>Create note</button>
+      </form>
+
+
+      {editNote && (
+        <form onSubmit={updateHandeler}>
+          <input name='newDescription' type="text" placeholder='Write new description' />
+          <button>Update</button>
+        </form>
+      )}
+
       <div className="notes">
         {
           notes.map((el, idx) => {
             return <div key={idx} className="note">
               <h1>{el.title}</h1>
               <p>{el.description}</p>
+              <button onClick={() => setEditNote(el._id)}>edit</button>
+              <button onClick={() => deleteHandeler(el._id)}> Delete</button>
             </div>
           })
         }
