fdc08cd7065fd3d47f16772364ed43e148eaa5ac
Nikhil-11-11-2007 <ng364357@gmail.com>
2026-02-04 15:07:42 +0530

    refactor: remove unused path import and cleanup unused route

--- Day9/Backend/src/app.js
+++ Day9/Backend/src/app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const noteModel = require("./models/note.model")
 const cors = require("cors")
-const path = require("path")
 
 const app = express()
 
@@ -54,10 +53,6 @@ app.patch("/api/notes/:id", async (req, res) => {
         message: "updated successfully",
         note
     })
-})
-
-app.use("*name", (req,res) => {
-    res.sendFile(path.join(__dirname, "..", "/public/index.html"))
 })
 
 module.exports = app
