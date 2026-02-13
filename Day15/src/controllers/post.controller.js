1cdad20d6bb5122322d745218e608dc3eb7053d1
Nikhil gupta <ng364357@gmail.com>
2026-02-13 16:53:47 +0530

    jwt verification

--- Day15/src/controllers/post.controller.js
+++ Day15/src/controllers/post.controller.js
@@ -2,7 +2,9 @@ const postModel = require("../models/post.model")
 
 const Imagekit = require("@imagekit/nodejs")
 
-const {toFile} = require("@imagekit/nodejs")
+const { toFile } = require("@imagekit/nodejs")
+
+const jwt = require("jsonwebtoken")
 
 const client = new Imagekit({
     privateKey: process.env.IMAGEKIY_PRIVATE_KEY
@@ -11,12 +13,45 @@ const client = new Imagekit({
 async function createPostController(req, res) {
     console.log(req.body, req.file);
 
+    const token = req.cookies.token
+
+    if (!token) {
+        res.status(401).json({
+            message: "Token not provided, Unauthorized access"
+        })
+    }
+
+    let decode = null
+
+    try {
+        decode = jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            message: "user not Authorize"
+        })
+    }
+
+    console.log(decode);
+    
+
     const file = await client.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder: "insta-clone"
     })
 
-    res.send(file)
+    console.log(file);
+    
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user:decode.id
+    })
+
+    res.status(201).json({
+        message:"post created",
+        post
+    })
 
 }
 
