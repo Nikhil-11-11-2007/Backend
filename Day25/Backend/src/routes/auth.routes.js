188232b17577eb45f1c9660cb213ec5553fc6879
Nikhil gupta <ng364357@gmail.com>
2026-02-28 16:39:36 +0530

    implement token blacklisting

--- Day25/Backend/src/routes/auth.routes.js
+++ Day25/Backend/src/routes/auth.routes.js
@@ -1,10 +1,15 @@
-const {Router} = require("express")
-const { registerUser, loginUser } = require("../controllers/auth.controller")
+const { Router } = require("express")
+const { registerUser, loginUser, getUser, logoutUser } = require("../controllers/auth.controller")
+const authUser = require("../middlewares/auth.middleware")
 
 const router = Router()
 
 router.post("/register", registerUser)
 
 router.post("/login", loginUser)
+
+router.get("/get-me", authUser ,getUser)
+
+router.get("/logout", logoutUser)
 
 module.exports = router
