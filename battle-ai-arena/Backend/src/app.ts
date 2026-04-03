1fe33c3cd448624fe6ba6f3602c447e511639a2b
Nikhil gupta <ng364357@gmail.com>
2026-04-03 14:22:00 +0530

    feat: initialize backend with TypeScript and Express

---
+++ battle-ai-arena/Backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import runGraph from "./ai/graph.ai.js"
+
+const app = express()
+
+app.get("/", async (req, res) => {
+
+    const result = await runGraph("Write an code for Factorial function in js")
+
+    res.json(result)
+    console.log(result);
+    
+})
+
+export default app
