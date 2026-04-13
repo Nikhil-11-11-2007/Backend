2f8de494b87e2056b69555de8c11588cb8e99a4a
Nikhil gupta <ng364357@gmail.com>
2026-04-13 15:48:04 +0530

    feat: add MongoDB connection setup and integrate Mongoose for database management

--- battle-ai-arena/Backend/src/config/config.ts
+++ battle-ai-arena/Backend/src/config/config.ts
@@ -2,10 +2,15 @@ import dotenv from "dotenv"
 
 dotenv.config()
 
+if(!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in environment variables");
+}
+
 const config = {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '',
     MISTRALAI_API_KEY: process.env.MISTRAL_API_KEY || '',
     COHERE_API_KEY: process.env.COHERE_API_KEY || '',
+    MONGO_URI: process.env.MONGO_URI,
 }
 
 
