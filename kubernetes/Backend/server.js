86081d0f1029b5206ba4bc9661a438fef32757df
Nikhil gupta <ng364357@gmail.com>
2026-05-07 16:23:19 +0530

    feat: update Kubernetes configurations for main-server deployment, ingress, and service

--- kubernetes/Backend/server.js
+++ kubernetes/Backend/server.js
@@ -1,20 +1,23 @@
-import express from 'express';
-import morgan from 'morgan';
-
+import express from "express";
+import morgan from "morgan";
 
 const app = express()
 
-app.use(morgan('dev'))
+app.use(morgan("dev"))
+app.use(express.json())
 
 app.get("/", (req, res) => {
-
-    let sum = 0
+    let sum = 0;
     for (let i = 0; i < 1000000000; i++) {
         sum += i
     }
-    res.send(`hello world sum is ${sum}`)
+
+    res.status(200).json({
+        message: "Sum calculated successfully",
+        sum
+    })
 })
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000`)
+    console.log("Server is running on port 3000")
 })
